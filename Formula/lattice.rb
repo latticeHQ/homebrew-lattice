eff1d4b1380b32a8355d403bcd0896cfbd211add
Lattice CI <ci@latticeruntime.com>
2026-02-08 18:37:22 +0530

    lattice 0.10.2

--- Formula/lattice.rb
+++ Formula/lattice.rb
@@ -4,27 +4,27 @@
 class Lattice < Formula
   desc "A self-hosted remote development platform"
   homepage "https://latticeruntime.com"
-  version "0.8.1"
+  version "0.10.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_darwin_arm64.zip"
-      sha256 "b3676a236f520f2c9f730020850af2c3343ad88954e9dbb57016ae4427128c87"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.10.2/lattice_0.10.2_darwin_arm64.zip"
+      sha256 "0efb02434c644524a1dac08a150b7ef8214f1e52daf11bd1d2d84ca27d77ff0f"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_darwin_amd64.zip"
-      sha256 "377f299d4d916278831a58a92616dc7fa77442983d5661ef03742aebc84fd1f3"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.10.2/lattice_0.10.2_darwin_amd64.zip"
+      sha256 "ca69e074c87a186fe32e2363a6fac6527fcae50b6c390188d5ed114a8da11724"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_linux_arm64.tar.gz"
-      sha256 "bce522dc5814a4e3bc596adba4944b416cbe5a69adaf7ae93f6eab4eee195d3d"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.10.2/lattice_0.10.2_linux_arm64.tar.gz"
+      sha256 "e5560487d8cefba5672687b3384f28d72b30982a59caef71a94e3c615e710e2c"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_linux_amd64.tar.gz"
-      sha256 "4bb9322582e43046e73ef98944f7044550bdcd4b886e9ae89f393f1db9a10750"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.10.2/lattice_0.10.2_linux_amd64.tar.gz"
+      sha256 "440e67b0619ca5d5ce3327fbf74b22bf24b6dcfb62a110d7db06b6eeeaa44dbd"
     end
   end
 
