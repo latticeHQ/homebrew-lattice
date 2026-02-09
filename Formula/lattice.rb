2ef0422beed99089c83126e6de67a22c4ec4fc68
Lattice CI <ci@latticeruntime.com>
2026-02-09 09:30:14 +0530

    lattice 0.11.0

--- Formula/lattice.rb
+++ Formula/lattice.rb
@@ -4,27 +4,27 @@
 class Lattice < Formula
   desc "A self-hosted remote development platform"
   homepage "https://latticeruntime.com"
-  version "0.8.1"
+  version "0.11.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_darwin_arm64.zip"
-      sha256 "b3676a236f520f2c9f730020850af2c3343ad88954e9dbb57016ae4427128c87"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.0/lattice_0.11.0_darwin_arm64.zip"
+      sha256 "4d335c555bff0a55476e4ba61f5134c21086b6eaa47664cb22c8d43d3d238a5a"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_darwin_amd64.zip"
-      sha256 "377f299d4d916278831a58a92616dc7fa77442983d5661ef03742aebc84fd1f3"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.0/lattice_0.11.0_darwin_amd64.zip"
+      sha256 "9795e06395ef65884b3572f730373e663bb5c53fabb16f1b17bc2da9dbcc4d66"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_linux_arm64.tar.gz"
-      sha256 "bce522dc5814a4e3bc596adba4944b416cbe5a69adaf7ae93f6eab4eee195d3d"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.0/lattice_0.11.0_linux_arm64.tar.gz"
+      sha256 "b0daea5c6d2b3cbffbe7a9fe3de1c2df0f9a065b517019084bc2d576e9a3d8d8"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_linux_amd64.tar.gz"
-      sha256 "4bb9322582e43046e73ef98944f7044550bdcd4b886e9ae89f393f1db9a10750"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.0/lattice_0.11.0_linux_amd64.tar.gz"
+      sha256 "d50c4e0578bed355aa12560e249c5a4b2330008eed719a6f39de99fedf97a745"
     end
   end
 
